b8b8e54549e39622e96062de3cb30812d2874e33
Mert Topal <mert.topal@opinnate.com>
2026-01-07 11:52:39 +0300

    rewrite IBUV action

--- src/modules/GatewayServer/actions/SERVER_GATEWAY_LOGIN_IBUV_CHALLENGE.ts
+++ src/modules/GatewayServer/actions/SERVER_GATEWAY_LOGIN_IBUV_CHALLENGE.ts
@@ -1,46 +1,45 @@
-import { Packet } from "@/core/Packet";
+import { Packet, StaticTypes } from "@/core";
+import { StaticType } from "@/core/types";
+const { bool, byte, int, short, string, stringASCII } = StaticTypes;
 
 export class SERVER_GATEWAY_LOGIN_IBUV_CHALLENGE extends Packet {
     static opcode = 0x2322;
     constructor() { super(SERVER_GATEWAY_LOGIN_IBUV_CHALLENGE.opcode); }
 
-    ImageCompressed: number;
-    ImageCompressedData: number[] = [];
+    ImageCompressed = short();
 
-    ImageFlag: number;
-    ImageHeight: number;
-    ImageRemain: number;
-    ImageUncompressed: number;
-    ImageWidth: number;
+    ImageFlag = byte();
+    ImageHeight = short();
+    ImageRemain = short();
+    ImageUncompressed = short();
+    ImageWidth = short();
 
     Read() {
-        this.ImageFlag = this.reader.uint8();
-        this.ImageRemain = this.reader.uint16();
-        this.ImageCompressed = this.reader.uint16();
-        this.ImageUncompressed = this.reader.uint16();
-        this.ImageWidth = this.reader.uint16();
-        this.ImageHeight = this.reader.uint16();
+        this.TryRead(this.ImageFlag);
+        this.TryRead(this.ImageRemain);
+        this.TryRead(this.ImageCompressed);
+        this.TryRead(this.ImageUncompressed);
+        this.TryRead(this.ImageWidth);
+        this.TryRead(this.ImageHeight)
 
         const remainingRead = this.RemainingRead();
-        for (let i = 0; i < remainingRead; i++) {
-            this.ImageCompressedData[i] = this.reader.uint8();
-        }
-
+        for (let i = 0; i < remainingRead; i++)
+            this.TryRead(byte())
     }
 
     Build() {
         this.Reset();
 
-        this.writer.uint8(this.ImageFlag);
-        this.writer.uint16(this.ImageRemain);
-        this.writer.uint16(this.ImageCompressed);
-        this.writer.uint16(this.ImageUncompressed);
-        this.writer.uint16(this.ImageWidth);
-        this.writer.uint16(this.ImageHeight);
-        for (let i = 0; i < this.ImageCompressedData.length; i++) {
-            this.writer.uint8(this.ImageCompressedData[i]);
-        }
-        
+        // this.writer.uint8(this.ImageFlag);
+        // this.writer.uint16(this.ImageRemain);
+        // this.writer.uint16(this.ImageCompressed);
+        // this.writer.uint16(this.ImageUncompressed);
+        // this.writer.uint16(this.ImageWidth);
+        // this.writer.uint16(this.ImageHeight);
+        // for (let i = 0; i < this.ImageCompressedData.length; i++) {
+        //     this.writer.uint8(this.ImageCompressedData[i]);
+        // }
+
         return this;
     }
 }
