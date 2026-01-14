669cb6d1270d8496c5a1ae8362af184d0f5c5165
Mert Topal <mert.topal@opinnate.com>
2026-01-14 14:59:07 +0300

    AuthGatewayServer Usage.

--- index.ts
+++ index.ts
@@ -2,12 +2,19 @@ import { FTemir } from '@/FTemir';
 import GatewayServer from '@/modules/GatewayServer/GatewayServer';
 import AgentServer from '@/modules/AgentServer/AgentServer';
 import { AuthServer } from '@/utils/auth-server/AuthServer';
+import AuthGatewayServer from '@/modules/AuthGatewayServer/AuthGatewayServer';
 
 const main = async () => {
   await FTemir.Initalize();
 
   const gatewayServer = new GatewayServer();
   gatewayServer.start();
+  
+
+  // If sro_client has an authentication code package
+  // const authGatewayServer = new AuthGatewayServer();
+  // authGatewayServer.start()
+
 
   new AgentServer().start();
   const authServer = new AuthServer();
